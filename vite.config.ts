a397aeb99a4ece2fd2e4134d6ddd9e5ead8a8e8c
Abdallah Gabr <133692606+a-amin-gabr@users.noreply.github.com>
2026-09-05 23:46:28 +0300

    fix: allow all hosts in vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   envDir: "../",
+  preview: {
+    allowedHosts: true,
+  },
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
